0542dfb84c87452362ae96026761e679f430ada0
lemodoescoding <119138020+lemodoescoding@users.noreply.github.com>
2024-01-06 22:01:51 +0700

    Added mason.nvim, lspconfig, nvim-cmp, and plenary.nvim ,also changed nvim-cmp.lua file to cmp.lua

--- lua/muj/plugins/lsp/mason.lua
+++ lua/muj/plugins/lsp/mason.lua
@@ -1,2 +1,35 @@
 return {
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim"
+  },
+
+  config = function()
+    local mason = require("mason")
+
+    local mason_lspconfig = require("mason-lspconfig")
+
+    mason.setup({
+      ui = {
+        icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+        },
+      },
+    })
+
+    mason_lspconfig.setup({
+      ensure_installled = {
+        "tsserver",
+        "cssls",
+        "html",
+        "lua_ls",
+        "emmet_ls",
+        "graphql"
+      },
+
+      automatic_installation = true,
+    })
+  end,
 }
